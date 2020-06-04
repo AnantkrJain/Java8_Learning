60ab643d215b244e880c7a317fb1defb76c1407a
AnantkrJain <anantkr.developer@gmail.com>
2020-06-04 09:26:12 +0530

    Working with Eclipse Environment

---
+++ src/org/imarticus/fsd/may30/Hello.java
@@ -0,0 +1,15 @@
+package org.imarticus.fsd.may30;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		int num=10;
+		System.out.println(num);
+		
+		//ctrl + spae = code completion
+		//ctrl + n = new cretion
+		//syso ctrl + space
+		//main ctrl + space
+		//run = ctrl + F11
+	}
+}
